241750e1eccc66001a74bcb1fa1acb8bea9e07bf
Shahid Ali <153486921+shahidali54@users.noreply.github.com>
2025-03-27 00:21:01 +0500

    4th-assignment

---
+++ 4th-assignment/my_module.py
@@ -0,0 +1,11 @@
+# A simple function
+def greet(name):
+    return f"Hello, {name}! Welcome to custom modules."
+
+# Another function
+def add(a, b):
+    return a + b
+
+
+# A variable
+pi_value = 3.14159
