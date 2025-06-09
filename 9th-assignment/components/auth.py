06cdaf3649fd5264682f322afb39d1cc1458c79c
Shahid Ali <153486921+shahidali54@users.noreply.github.com>
2025-06-09 10:08:29 +0500

    Update auth.py

--- 9th-assignment/components/auth.py
+++ 9th-assignment/components/auth.py
@@ -50,4 +50,5 @@ class AuthService:
     
     def logout(self):
         st.session_state.user = None
-        st.session_state.current_page = "auth" 
+        st.session_state.current_page = "auth"
+        
